af2d30fad160b9b42428db69549ef9040111e8a9
parkero2 <parkero2@stu.otc.school.nz>
2021-03-12 11:54:59 +1300

    Added giftcard Oliver.py
    A program that calculates the remaining balance on a giftcard

---
+++ giftcard Oliver.py
@@ -0,0 +1,4 @@
+initial_value = 50 # The starting value of a giftcard
+amount_spent = 35 # How much was spent
+
+print("You spent ${} of your ${} giftcard, leaving ${} remaining.".format(amount_spent, initial_value, initial_value - amount_spent))
